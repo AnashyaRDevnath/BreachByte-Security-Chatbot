89b32ffd3dd37dd8208849d402da9a5a75cc649c
AnashyaRDevnath <anashyard14@gmail.com>
2026-03-30 20:58:38 +0200

    Fixed emoji display bug
    -The console displayed the emoji as '??', so I updated the console encoding to UTF-8

--- BreachByte_SecurityBot/BreachByte_SecurityBot/Program.cs
+++ BreachByte_SecurityBot/BreachByte_SecurityBot/Program.cs
@@ -4,16 +4,14 @@
     {
         static void Main(string[] args)
         {
-            /* ---PSEUDO CODE---
-               CALL UserInterface to START_GREETING
-              CALL BotBrain to START_CONVERSATION_LOOP
-              END PROGRAM
-            
-             */
+            //For app to display to emojis
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            //https://stackoverflow.com/questions/67508469/how-to-show-emoji-in-c-sharp-console-output miken32
+
 
             //Voice greeting
-           //AudioPlayer audio = new AudioPlayer();
-           // audio.PlayVoiceGreeting ();
+            AudioPlayer audio = new AudioPlayer();
+             audio.PlayVoiceGreeting ();
 
             //Ascii logo
             UserInterface logo = new UserInterface();
--- BreachByte_SecurityBot/BreachByte_SecurityBot/obj/Debug/net10.0/BreachByte_SecurityBot.AssemblyInfo.cs
+++ BreachByte_SecurityBot/BreachByte_SecurityBot/obj/Debug/net10.0/BreachByte_SecurityBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d46c9b4d75a780b1229f97b1b98113805eb4b07b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1423896de82d22c7d07bc3a555a9a812f174186a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
