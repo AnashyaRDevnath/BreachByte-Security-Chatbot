9f28418d06b93bb606c8290bff1ecae857ef5554
AnashyaRDevnath <anashyard14@gmail.com>
2026-06-19 23:16:17 +0200

    Added MySql.Data dependency via NuGet for database integration

--- BreachByte_SecurityBot/BreachByte_SecurityBot/obj/Debug/net10.0-windows/BreachByte_SecurityBot.AssemblyInfo.cs
+++ BreachByte_SecurityBot/BreachByte_SecurityBot/obj/Debug/net10.0-windows/BreachByte_SecurityBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b8b2de1c48559e561cfc5b76a766275061d854")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad6d26d7af22042e018f162ac1ca4db309e9db2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
