1c268c49193b5cac5a0dcb6911c6cea23d760173
AnashyaRDevnath <anashyard14@gmail.com>
2026-06-20 19:45:33 +0200

    Generated MySQL database schema and table structures
    -Built BreachByteDB in MySQL Workbench.
    -Created cyber_tasks table with CRUD compatibility.
    -Created activity_logs table for timestamped system auditing.
    -Saved SQL script to repository for version control and lecturer review.

---
+++ BreachByte_SecurityBot/BreachByte_SecurityBot/DatabaseSchema.sql
@@ -0,0 +1,19 @@
+﻿CREATE SCHEMA BreachByteDB;
+USE BreachByteDB;
+
+-- Create the Tasks table
+CREATE TABLE cyber_tasks (
+    task_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    reminder_date DATETIME NULL,
+    is_completed BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create the Activity Log table
+CREATE TABLE activity_logs (
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    action_description VARCHAR(255) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
--- BreachByte_SecurityBot/BreachByte_SecurityBot/obj/Debug/net10.0-windows/BreachByte_SecurityBot.AssemblyInfo.cs
+++ BreachByte_SecurityBot/BreachByte_SecurityBot/obj/Debug/net10.0-windows/BreachByte_SecurityBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f28418d06b93bb606c8290bff1ecae857ef5554")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0e0fbce84aa8483d96e2b3178c39d88ab661f97")]
 [assembly: System.Reflection.AssemblyProductAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BreachByte_SecurityBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
